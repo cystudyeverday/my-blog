ddc21282eb32a5182b01d85178639f57770c0e41
caiyang <373805195@qq.com>
2026-01-09 08:20:16 +0000

    config file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
